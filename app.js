74e9bbc4ef2b8e308c5edac0a16061cb796b929f
hwakyung <hwakyung5531@gmail.com>
2022-08-19 21:17:16 +0900

    proj: app.js 생성
    npm 실행 파일

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+const indexRouter = require("./routes");
+const connect = require("./schemas");
+
+dotenv.config();
+
+const app = express();
+app.set("port", process.env.PORT || 18923);
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use("/", indexRouter);
+
+connect();
+
+app.listen(app.get("port"), () => {
+    console.log(app.get("port"), '번 포트에서 대기 중');
+});

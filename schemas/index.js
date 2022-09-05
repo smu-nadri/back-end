df9d79089a9f4cac66a541bef0c48d91923ce398
hwakyung <hwakyung5531@gmail.com>
2022-09-05 16:17:12 +0900

    앨범 검색 수정, 데이터베이스uri 변경

--- schemas/index.js
+++ schemas/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connect = () => {
-    mongoose.connect(process.env.DATABASE_URI, {
+    mongoose.connect(process.env.MONGO_URI, {
         dbName: "testDB",
         useNewUrlParser: true
     })

c1a39817b2bd33f4ed98e6eebd54c4c26005550b
hwakyung <hwakyung5531@gmail.com>
2022-10-03 14:17:05 +0900

    proj: DB 에러, 재연결 코드 추가

--- schemas/index.js
+++ schemas/index.js
@@ -3,10 +3,16 @@ const mongoose = require("mongoose");
 const connect = () => {
     mongoose.connect(process.env.MONGO_URI, {
         dbName: "testDB",
-        useNewUrlParser: true
+        useNewUrlParser: true,
     })
     .then(() => console.log("Connection to CosmodDB"))
     .catch((err) => console.error(err));
-}
+};
+mongoose.connection.on('error', (error) => {
+    console.error('MongoDB Connection Error :', error);
+});
+mongoose.connection.on('disconnected', () => {
+    console.error("MongoDB DisConnected. Retry Connection");
+});
 
 module.exports = connect;

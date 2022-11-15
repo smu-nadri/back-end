63cfacb26dbf269d15908f2d69687f88b5bd9510
hwakyung <hwakyung5531@gmail.com>
2022-11-15 23:54:48 +0900

    차트 지역 조건 반대로 설정함

--- routes/stats.js
+++ routes/stats.js
@@ -182,7 +182,7 @@ router.get("/:id", async (req, res) => {
             { 
                 $group: {
                     _id: "$uri",
-                    locality: { $first: { $cond: [ { $eq: [ "$location.locality", null ] }, "$location.locality", "$location.admin" ] } },
+                    locality: { $first: { $cond: [ { $eq: [ "$location.locality", null ] }, "$location.admin", "$location.locality" ] } },
                     thoroughfare: { $first: "$location.thoroughfare" },
                     count: { $sum: 1 }
                 } 
